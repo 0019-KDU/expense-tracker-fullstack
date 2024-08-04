7f401f9663f5dc6a71a7b90098077318fb0a77f6
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-08-04 11:59:32 +0530

    finsh the User Resolvers
    signup,login and logout functions

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,12 +1,93 @@
+import { Query } from "mongoose";
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
-  Query: {
-    users: () => {
-      return users;
+  Mutation: {
+    signup: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required");
+        }
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("Username already exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(newUser);
+        return newUser;
+      } catch (err) {
+        console.log("Error in signup", err);
+        throw new Error(err.message || "Internal Server Error");
+      }
+    },
+
+    login: async (_, { input }, context) => {
+      try {
+        const { username, password } = input;
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          password,
+        });
+        await context.login(user);
+      } catch (err) {
+        console.log("Error in login", err);
+        throw new Error(err.message || "Internal Server Error");
+      }
+    },
+    logout: async (_, __, context) => {
+      try {
+        await context.logout();
+        req.session.destory((err) => {
+          if (err) {
+            throw err;
+          }
+        });
+        res.clearCookie("connect.sid");
+        return { message: "Logged Out Successfully" };
+      } catch (err) {
+        console.log("Error in logout", err);
+        throw new Error(err.message || "Internal Server Error");
+      }
     },
   },
-  Mutation: {},
+  Query: {
+    authUser: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        return user;
+      } catch (err) {
+        console.error("Error in authuser", err);
+        throw new Error("Internal Server Error");
+      }
+    },
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (err) {
+        console.error("Error in user", err);
+        throw new Error("Internal Server Error");
+      }
+    },
+  },
 };
 
 export default userResolver;
